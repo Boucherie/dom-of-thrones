b3f5497f233b1a63582cbfdf070869c5f3812dcf
Emily Boucher <emi.boucher@gmail.com>
2017-07-08 00:05:14 -0400

    finished adding ned into scene 2

--- js/scene2.js
+++ js/scene2.js
@@ -13,18 +13,31 @@ function scene2() {
 
   nedContainer = document.createElement('div')
   stage.append(nedContainer)
-  nedContainer.style.border = '2px solid red'
-  nedContainer.style.backgroundColor = 'yellow'
+  // nedContainer.style.border = '2px solid red'
+  // nedContainer.style.backgroundColor = 'yellow'
   nedContainer.style.position = 'absolute'
   nedContainer.style.width = '10%'
   nedContainer.style.height = '20%'
   nedContainer.style.top = '50%'
-  nedContainer.style.left = '50%'
+  nedContainer.style.left = '25%'
 
   ned = document.querySelector('#ned')
   nedContainer.append(ned)
 
-  
+  leather = document.querySelector('#leather-armour')
+  nedContainer.append(leather)
+  // ned.style.border = '2px solid blue'
+
+  // leather.style.border = '2px solid green'
+  leather.style.position = 'absolute'
+  leather.style.bottom = '0'
+  leather.style.left = '0'
+
+  nedContainer.style.height = '47%'
+  ned.style.width = '30px'
+  nedContainer.style.height = '37%'
+  ned.style.position = 'absolute'
+  ned.style.left = '4px'
   // Character 2
 
   // Setup Props
