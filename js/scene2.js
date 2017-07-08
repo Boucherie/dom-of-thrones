4d2e5c526b842f45435e1e9d75ea154b42b05f01
Emily Boucher <emi.boucher@gmail.com>
2017-07-07 23:48:12 -0400

    set stage, created nedContainer and placed headshot inside

--- js/scene2.js
+++ js/scene2.js
@@ -1,12 +1,30 @@
 function scene2() {
-  console.log('Executing scene 2!');
+  // console.log('Executing scene 2!');
 
   // Setup Stage
+  castle = document.querySelector('#castle')
+  stageImg = stage.querySelector('#stage-img')
+  castleImgSrc = castle.getAttribute('src')
+  stageImg.setAttribute('src', castleImgSrc)
 
   // Setup Cast
 
   // Character 1
 
+  nedContainer = document.createElement('div')
+  stage.append(nedContainer)
+  nedContainer.style.border = '2px solid red'
+  nedContainer.style.backgroundColor = 'yellow'
+  nedContainer.style.position = 'absolute'
+  nedContainer.style.width = '10%'
+  nedContainer.style.height = '20%'
+  nedContainer.style.top = '50%'
+  nedContainer.style.left = '50%'
+
+  ned = document.querySelector('#ned')
+  nedContainer.append(ned)
+
+  
   // Character 2
 
   // Setup Props
