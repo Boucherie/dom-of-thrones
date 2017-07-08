b3413b3804245d6e80f6dd87d9ad55a4d37396f1
Emily Boucher <emi.boucher@gmail.com>
2017-07-08 00:13:31 -0400

    finihsed positioning ned

--- js/scene2.js
+++ js/scene2.js
@@ -13,31 +13,32 @@ function scene2() {
 
   nedContainer = document.createElement('div')
   stage.append(nedContainer)
-  // nedContainer.style.border = '2px solid red'
+  nedContainer.style.border = 'none'
   // nedContainer.style.backgroundColor = 'yellow'
   nedContainer.style.position = 'absolute'
   nedContainer.style.width = '10%'
   nedContainer.style.height = '20%'
-  nedContainer.style.top = '50%'
-  nedContainer.style.left = '25%'
+  nedContainer.style.top = '55%'
+  nedContainer.style.left = '10%'
 
   ned = document.querySelector('#ned')
   nedContainer.append(ned)
 
   leather = document.querySelector('#leather-armour')
   nedContainer.append(leather)
-  // ned.style.border = '2px solid blue'
+  ned.style.border = 'none'
 
-  // leather.style.border = '2px solid green'
+  leather.style.border = 'none'
   leather.style.position = 'absolute'
   leather.style.bottom = '0'
   leather.style.left = '0'
 
-  nedContainer.style.height = '47%'
+  // nedContainer.style.height = '47%'
   ned.style.width = '30px'
   nedContainer.style.height = '37%'
   ned.style.position = 'absolute'
   ned.style.left = '4px'
+
   // Character 2
 
   // Setup Props
